2204d400e98098c4b7628d7d4efeb3e11e65f529
Michael Goodness <mgood@gbpinc.com>
2015-03-12 14:56:56 -0500

    Git role: use ansible_user_id as default value for username, homedir, email_address, and full_name.

--- config.sh
+++ config.sh
@@ -6,15 +6,6 @@ elif [ $HOSTNAME == 'wintermute' ]; then
 	ansible-playbook ~/configs/ansible/laptop.yml --ask-sudo-pass -v
 fi	
 
-# Git
-ln -sf ~/configs/gitconfig ~/.gitconfig
-ln -sf ~/configs/gitignore_global ~/.gitignore_global
-
-# Zsh
-curl -L http://install.ohmyz.sh | sh
-ln -sf ~/configs/zshrc ~/.zshrc
-chsh -s /usr/bin/zsh
-
 # Sublime Text
 mkdir -p ~/.config/sublime-text-3/Packages
 ln -sf ~/configs/sublime-text-3 ~/.config/sublime-text-3/Packages/User
@@ -22,4 +13,3 @@ ln -sf ~/configs/sublime-text-3 ~/.config/sublime-text-3/Packages/User
 # SSH
 rm -rf ~/.ssh
 ln -sf ~/configs/ssh ~/.ssh
-
