8efb7d2c16b8bc2b1d4dc4f2921073fefc7518cd
Michael Goodness <mgood@gbpinc.com>
2015-03-27 13:01:03 -0500

    Added ST3 license file, encrypted product keys with Vault

--- config.sh
+++ config.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [ $HOSTNAME == 'mgood-desktop' ]; then
-	ansible-playbook ~/configs/ansible/desktop.yml --ask-sudo-pass -v
-elif [ $HOSTNAME == 'wintermute' ]; then
-	ansible-playbook ~/configs/ansible/laptop.yml --ask-sudo-pass -v
+	ansible-playbook ~/configs/ansible/desktop.yml -e @~/configs/ansible/keys.yml --ask-vault-pass --ask-sudo-pass -v
+elif [ $HOSTNAME == 'neuromancer' ]; then
+	ansible-playbook ~/configs/ansible/laptop.yml -e @~/configs/ansible/keys.yml --ask-vault-pass --ask-sudo-pass -v
 fi	
 
 # Sublime Text
-mkdir -p ~/.config/sublime-text-3/Packages
-ln -sf ~/configs/sublime-text-3 ~/.config/sublime-text-3/Packages/User
+mkdir -p ~/.config/sublime-text-3/Local
+cp License.sublime_license ~/.config/sublime-text-3/Local/
 
 # SSH
 rm -rf ~/.ssh
