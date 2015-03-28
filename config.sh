e4970505d8593aeff0364231c8948bf9eabd658f
Michael Goodness <mgoodness@gmail.com>
2015-03-27 22:05:29 -0500

    Everything's working

--- config.sh
+++ config.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 if [ $HOSTNAME == 'mgood-desktop' ]; then
-	ansible-playbook ~/configs/ansible/desktop.yml -e @~/configs/ansible/keys.yml --ask-vault-pass --ask-sudo-pass -v
+	ansible-playbook configs/ansible/desktop.yml -e @configs/ansible/keys.yml --ask-vault-pass --ask-sudo-pass -v
 elif [ $HOSTNAME == 'neuromancer' ]; then
-	ansible-playbook ~/configs/ansible/laptop.yml -e @~/configs/ansible/keys.yml --ask-vault-pass --ask-sudo-pass -v
+	ansible-playbook configs/ansible/laptop.yml -e @configs/ansible/keys.yml --ask-vault-pass --ask-sudo-pass -v
 fi	
 
 # Sublime Text
 mkdir -p ~/.config/sublime-text-3/Local
-cp License.sublime_license ~/.config/sublime-text-3/Local/
+cp ~/configs/License.sublime_license ~/.config/sublime-text-3/Local/
 
 # SSH
-rm -rf ~/.ssh
-ln -sf ~/configs/ssh ~/.ssh
+#rm -rf ~/.ssh
+#ln -sf ~/configs/ssh ~/.ssh
+
