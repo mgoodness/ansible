b106a708030b9a10661b2328d7508bb4c0b1ef59
Michael Goodness <mgoodness@gmail.com>
2015-04-01 22:33:04 -0500

    Added wintermute host, renamed desktop & laptop

--- config.sh
+++ config.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 if [ $HOSTNAME == 'mgood-desktop' ]; then
-	ansible-playbook ~/configs/ansible/desktop.yml -e @~/configs/ansible/keys.yml --ask-vault-pass --ask-sudo-pass -v
+	ansible-playbook ~/configs/ansible/mgood-desktop.yml -e @~/configs/ansible/keys.yml --ask-vault-pass --ask-sudo-pass -v
 elif [ $HOSTNAME == 'neuromancer' ]; then
-	ansible-playbook ~/configs/ansible/laptop.yml -e @~/configs/ansible/keys.yml --ask-vault-pass --ask-sudo-pass -v
+	ansible-playbook ~/configs/ansible/neuromancer.yml -e @~/configs/ansible/keys.yml --ask-vault-pass --ask-sudo-pass -v
+elif [ $HOSTNAME == 'wintermute' ]; then
+	ansible-playbook ~/configs/ansible/wintermute.yml -e @~/configs/ansible/keys.yml --ask-vault-pass --ask-sudo-pass -v
 fi	
 
 # Sublime Text
