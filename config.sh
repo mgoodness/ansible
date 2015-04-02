267f78aa1571612ac20fd02d63e81a5aa859fbab
Michael Goodness <mgood@gbpinc.com>
2015-04-02 13:10:45 -0500

    Removed Sublime Text license.

--- config.sh
+++ config.sh
@@ -2,19 +2,8 @@
 
 KEYFILE="@$HOME/configs/ansible/keys.yml"
 
-if [ $HOSTNAME == 'mgood-desktop' ]; then
-	ansible-playbook ~/configs/ansible/mgood-desktop.yml -e $KEYFILE --ask-vault-pass --ask-sudo-pass -v
-elif [ $HOSTNAME == 'neuromancer' ]; then
-	ansible-playbook ~/configs/ansible/neuromancer.yml -e $KEYFILE --ask-vault-pass --ask-sudo-pass -v
-elif [ $HOSTNAME == 'wintermute' ]; then
-	ansible-playbook ~/configs/ansible/wintermute.yml -e $KEYFILE --ask-vault-pass --ask-sudo-pass -v
-fi	
-
-# Sublime Text
-mkdir -p ~/.config/sublime-text-3/Local
-cp ~/configs/License.sublime_license ~/.config/sublime-text-3/Local/
+ansible-playbook ~/configs/ansible/$HOSTNAME.yml -e $KEYFILE --ask-vault-pass --ask-sudo-pass -v
 
 # SSH
 #rm -rf ~/.ssh
 #ln -sf ~/configs/ssh ~/.ssh
-
