a829a4299c9fe9330b628224154ce8873a52de6c
Michael Goodness <mgood@gbpinc.com>
2015-04-02 14:00:48 -0500

    Moar cleanup

--- config.sh
+++ config.sh
@@ -2,6 +2,8 @@
 
 KEYFILE="@$HOME/configs/ansible/keys.yml"
 
+sudo yum install -y ansible
+
 ansible-playbook ~/configs/ansible/$HOSTNAME.yml --ask-sudo-pass -v -e $KEYFILE --ask-vault-pass 
 
 # SSH
